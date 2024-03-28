11bb7f6f2b6bbe6b152a8a4e44fc865bffbf974e
Zoee <30841158+n-WN@users.noreply.github.com>
2024-03-28 18:04:11 +0800

    Update campusNetLink.py

--- campusNetLink.py
+++ campusNetLink.py
@@ -14,7 +14,7 @@ cookies = {
 host = ''  # 登录页面地址    ---------- 3
 
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
-package = '学生-电信-100M'  # 套餐名称
+package = ''
 ip = ''  
 
 def load_headers(task):
