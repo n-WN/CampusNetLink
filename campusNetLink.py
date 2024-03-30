d1062bc53ab3337c28cffb861cfada95f7a5b6dd
Han_Qiong <1797752637@qq.com>
2024-03-30 17:41:26 +0800

    feat: 新增对已有上线设备时的兼容处理

--- campusNetLink.py
+++ campusNetLink.py
@@ -1,10 +1,13 @@
+import json
 import requests
 import logging
 import time
 import random
 import string
 
-logging.basicConfig(level=logging.INFO, format=f'\033[1;36m[*]\033[0m %(asctime)s - \033[1;32m%(levelname)s\033[0m - \033[1;32m%(message)s\033[0m')
+logging.basicConfig(level=logging.INFO,
+                    format=f'\033[1;36m[*]\033[0m %(asctime)s - \033[1;32m%(levelname)s\033[0m - \033[1;32m%('
+                           f'message)s\033[0m')
 
 cookies = {
     # 只需填入以下 2 处参数:
@@ -14,9 +17,11 @@ cookies = {
 
 host = 'http://1.1.1.1'
 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+user_agent = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 '
+              'Safari/537.36')
 package = ''
-ip = ''  
+ip = ''
+
 
 def load_headers(task):
     global headers
@@ -27,31 +32,36 @@ def load_headers(task):
         'Content-Type': 'application/json',
     }
     if task in ['登录', '上线']:
-        headers.update({'Cache-Control': 'no-cache', 'Connection': 'keep-alive', 'DNT': '1', 'Origin': host, 'Pragma': 'no-cache'})
+        headers.update(
+            {'Cache-Control': 'no-cache', 'Connection': 'keep-alive', 'DNT': '1', 'Origin': host, 'Pragma': 'no-cache'})
     elif task not in ['下线', '查询']:
         logging.error('未知任务: %s', task)
 
-def generate_asp_sessionid():
+
+def generate_asp_session_id():
     return ''.join(random.choices(string.ascii_lowercase + string.digits, k=24))
+
 
 def login_aspx():
     headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', # 预期响应内容类型
-        'accept-language': 'zh-CN,zh;q=0.9', # 语言偏好 | zh-CN 中文简体 优先 | 其次 zh 中文
-        'cache-control': 'no-cache', # 缓存机制 | 表示请求不应从缓存中获取信息
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
+                  'application/signed-exchange;v=b3;q=0.7',
+        # 预期响应内容类型
+        'accept-language': 'zh-CN,zh;q=0.9',  # 语言偏好 | zh-CN 中文简体 优先 | 其次 zh 中文
+        'cache-control': 'no-cache',  # 缓存机制 | 表示请求不应从缓存中获取信息
         'content-type': 'application/x-www-form-urlencoded',
-        'dnt': '1', # 用户的隐私偏好 - Do Not Track | 1 表示用户不希望他们的浏览活动被追踪
-        'origin': host, 
-        'pragma': 'no-cache', # 缓存控制 | 用于向后兼容HTTP/1.0缓存控制
+        'dnt': '1',  # 用户的隐私偏好 - Do Not Track | 1 表示用户不希望他们的浏览活动被追踪
+        'origin': host,
+        'pragma': 'no-cache',  # 缓存控制 | 用于向后兼容HTTP/1.0缓存控制
         'referer': f'{host}/basic.aspx',
         'sec-ch-ua': '"Google Chrome";v="75", "Not:A-Brand";v="99", "Chromium";v="75"',
-        'sec-ch-ua-mobile': '?0', # 设备类型 | 非移动设备
-        'sec-ch-ua-platform': '"Windows"', 
-        'sec-fetch-dest': 'document', # 文档 | 目的地类型: HTML 文档
-        'sec-fetch-mode': 'navigate', # 导航 | 请求导航到另一个页面
-        'sec-fetch-site': 'same-origin', # 同源 | 请求是从与目标资源同一域的页面发起的
-        'sec-fetch-user': '?1', # 触发方式 | 用户触发
-        'upgrade-insecure-requests': '1', # 升级 | 期望 https 请求
+        'sec-ch-ua-mobile': '?0',  # 设备类型 | 非移动设备
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',  # 文档 | 目的地类型: HTML 文档
+        'sec-fetch-mode': 'navigate',  # 导航 | 请求导航到另一个页面
+        'sec-fetch-site': 'same-origin',  # 同源 | 请求是从与目标资源同一域的页面发起的
+        'sec-fetch-user': '?1',  # 触发方式 | 用户触发
+        'upgrade-insecure-requests': '1',  # 升级 | 期望 https 请求
         'user-agent': user_agent,
     }
 
@@ -62,10 +72,11 @@ def login_aspx():
     data = cookies
 
     response = requests.post(f'{host}/basic.aspx', params=params, cookies={
-        'ASP.NET_SessionId': generate_asp_sessionid(),
+        'ASP.NET_SessionId': generate_asp_session_id(),
     }, headers=headers, data=data, verify=False)
 
     return response.text
+
 
 # 需要跟踪确认 hash 方式
 """
@@ -80,6 +91,7 @@ def login():
         return str(e)
 """
 
+
 def get_info():
     load_headers(task='查询')
     json_data = {'DoWhat': 'GetInfo'}
@@ -87,7 +99,7 @@ def get_info():
         response = requests.post(f'{host}/Auth.ashx', headers=headers, json=json_data)
         global ip
         try:
-            ip = response.json().get('Data', {}).get('OIA', [])[0].get('IP', '') # 常规获取第零项 ip
+            ip = response.json().get('Data', {}).get('OIA', [])[0].get('IP', '')  # 常规获取第零项 ip
         except Exception as e:  # 指定异常类型
             logging.error('获取 IP 时发生错误, 正在登陆: %s', e)
 
@@ -106,6 +118,7 @@ def get_info():
         logging.error('查询请求失败: %s', e)
         return str(e)
 
+
 def go_online():
     load_headers(task='上线')
     json_data = {'DoWhat': 'OpenNet', 'Package': package}
@@ -115,6 +128,7 @@ def go_online():
     except requests.RequestException as e:
         logging.error('上线请求失败: %s', e)
         return str(e)
+
 
 def go_offline():
     load_headers(task='下线')
@@ -127,12 +141,17 @@ def go_offline():
         logging.error('下线请求失败: %s', e)
         return str(e)
 
+
 if __name__ == '__main__':
     login_aspx()
     # 随机等待 1-3 秒, 避免可能的风控
     time.sleep(1 + 2 * random.random())
     get_info()
     time.sleep(1 + 2 * random.random())
-    print(go_online())
-    # time.sleep(1 + 2 * random.random())
-    # print(go_offline())
+    while True:
+        online_msg = json.loads(go_online())
+        if online_msg["Result"]:
+            print(online_msg["Message"])
+            break
+        else:
+            go_offline()
